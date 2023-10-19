2a5b526df68d5feb309214e15fbfd705692a885f
Phelipe Andre <94318251+Phelipe-Andre-Dev@users.noreply.github.com>
2023-10-18 23:43:11 -0300

    commit1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import styles from './Home.module.css'
-
+import { Link } from 'react-router-dom'
 import Footer from '../layout/Footer'
 
 function Home(){
@@ -8,9 +8,20 @@ function Home(){
             <div>
                 <h1>Home</h1>
                 <h2>Sejam Bem-Vindos a <h1 className={styles.bely}>BelyBeads</h1></h2>
-            
+               
+                <h3 className={styles.textohome}> Descubra o charme e exclusividade das pulseiras
+                    personalizadas da BelyBeads, crie sua própria pulseira, para usar em qualquer ocasião.
+
+                </h3>
                 
+
+                <h2>Siga também nossa página no Instagram <h3><a href="https://instagram.com/belybeadss?igshid=MzMyNGUyNmU2YQ%3D%3D&utm_source=qr">BelyBeads</a></h3>
+                    
+                </h2> 
+                
+                            
             </div>
+            <Link className={styles.btn} to='/produtos'>Produtos</Link>
             <div className={styles.f}><Footer /></div>
         </div>
     )
--- src/pages/Produtos.js
+++ src/pages/Produtos.js
@@ -1,11 +1,21 @@
 import styles from './Produtos.module.css'
 import Footer from '../layout/Footer'
+import { Link } from 'react-router-dom'
 
 
 import img2 from '../layout/img.2.jpg'
 import img3 from '../layout/img.3.jpg'
 import img4 from '../layout/img.4.jpg'
 import img5 from '../layout/img.5.jpg'
+import img6 from '../layout/img.6.jpg'
+import img7 from '../layout/img.7.jpg'
+import img8 from '../layout/img.8.jpg'
+import img9 from '../layout/img.9.jpg'
+import img10 from '../layout/img.10.jpg'
+import img11 from '../layout/img.11.jpg'
+import img12 from '../layout/img.12.jpg'
+
+
 
 
 
@@ -20,8 +30,23 @@ function Produtos (){
                 <img src={img3}/>
                 <img src={img4}/>
                 <img className={styles.img5} src={img5}/>
-                
-             </div>
+                <img className={styles.img6} src={img6}/>
+                <img src={img7}/>
+                <img src={img8}/>
+                <img src={img9}/>
+                <img src={img10}/>
+                <img src={img11}/>
+                <img src={img12}/>
+            </div>
+
+          <h3>Arraste para visualizar...</h3>
+
+          <article>
+            <h3 className={styles.textoprodutos}>
+                Crie sua própria pulseira, ou chaveiro, personalizados! 
+            </h3>
+          </article>
+          <Link className={styles.btn} to='/pedidos'>Compre Já</Link>
            
             <div className={styles.f}><Footer/></div>
         </div>
